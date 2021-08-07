22cae81259576f8d980c1a47c1fb032417be38d1
twinh <twinhuang@qq.com>
2021-08-07 23:40:56 +0800

    feat(m): 增加服务号前端登录操作
    - 跨域时默认无法获取完整 Referrer，需前台需传入要跳转的 URL

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -3,11 +3,17 @@ import {setApiUrl, setOnBeforeHttp} from '@mxjs/taro';
 import Taro from '@tarojs/taro';
 import $ from 'miaoxing';
 import '@/components/Page';
+import qs from 'query-string';
+import appendUrl from 'append-url';
 
 // bootstrap: 引入自定义的样式主题
 import 'fower-preset-miaoxing';
 
 const login = async () => {
+  if (process.env.TARO_ENV === 'h5') {
+    return loginWechatOa();
+  }
+
   // 1. 调用登录获取 code
   const res = await Taro.login();
   if (!res.code) {
@@ -32,6 +38,48 @@ const login = async () => {
 
   // 3. 记录 token
   Taro.setStorageSync('token', ret.token);
+};
+
+/**
+ * 微信服务号登录
+ */
+const loginWechatOa = async () => {
+  if (!navigator.userAgent.includes('MicroMessenger')) {
+    return;
+  }
+
+  if (Taro.getStorageSync('token')) {
+    return;
+  }
+
+  const code = $.req('code');
+  const state = $.req('state');
+
+  // 授权回调
+  if (code && typeof state !== 'undefined') {
+    const {ret} = await $.post({
+      url: $.apiUrl('wechat-oa/login'),
+      data: {code, state, url: window.location.href},
+    });
+    if (ret.isSuc()) {
+      Taro.setStorageSync('token', ret.token);
+      const {code, state, ...params} = qs.parse(window.location.search);
+      // 跳转移除当前地址中的 code 和 state 参数
+      // noinspection ES6MissingAwait 实际不会 resolve
+      Taro.redirectTo({
+        url: appendUrl(window.location.pathname, params),
+      });
+    }
+    return;
+  }
+
+  // 跳转到授权地址
+  const {ret} = await $.get($.apiUrl('wechat-oa/login', {url: window.location.href}));
+  if (ret.isErr()) {
+    // 忽略错误，例如未设置服务号
+    return;
+  }
+  window.location = ret.url;
 };
 
 const bootstrap = async () => {
