1d7d110d71bdbb6694c815beb8ab68b259060df1
twinh <twinhuang@qq.com>
2021-11-24 14:58:07 +0800

    feat(m): 配置改为读取根目录的统一配置
    BREAKING CHANGE: 配置改为读取根目录的统一配置

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -1,13 +1,13 @@
-import config from '@/config';
 import {setApiUrl, setOnBeforeHttp} from '@mxjs/taro';
 import '@/components/Page';
 // bootstrap: 引入自定义的样式主题
 import 'fower-preset-miaoxing';
 import login from './login';
+import config from '../../../config';
 
 const bootstrap = async () => {
   // 设置请求接口的地址
-  setApiUrl(config.apiUrl);
+  setApiUrl(config.url.baseApiUrl);
 
   // 请求登录
   const loginResult = login();
