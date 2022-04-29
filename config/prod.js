ad50a2e13d21e43505f128bbda262ec473950668
twinh <twinhuang@qq.com>
2022-04-29 15:20:19 +0800

    build(m): taro build 增加 hash 值

--- config/prod.js
+++ config/prod.js
@@ -2,10 +2,20 @@ module.exports = {
   env: {
     NODE_ENV: '"production"',
   },
-  defineConstants: {
-  },
+  defineConstants: {},
   mini: {},
   h5: {
+    output: {
+      filename: 'js/[name]-[chunkhash:6].js',
+      chunkFilename: 'js/[name]-[chunkhash:6].js',
+    },
+    imageUrlLoaderOption: {
+      name: 'static/images/[path][name]-[hash:6].[ext]',
+    },
+    miniCssExtractPluginOption: {
+      filename: 'css/[name]-[contenthash:6].css',
+      chunkFilename: 'css/[id]-[contenthash:6].css',
+    },
     /**
      * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
      * 参考代码如下：
