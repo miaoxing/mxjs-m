13119be45dd993cbbf2cbc5b1396a11880d5b6e7
twinh <twinhuang@qq.com>
2022-05-12 17:11:01 +0800

    build: 生成文件按目录区分

--- config/prod.js
+++ config/prod.js
@@ -6,15 +6,15 @@ module.exports = {
   mini: {},
   h5: {
     output: {
-      filename: 'js/[name]-[chunkhash:6].js',
-      chunkFilename: 'js/[name]-[chunkhash:6].js',
+      filename: 'm/js/[name]-[chunkhash:6].js',
+      chunkFilename: 'm/js/[name]-[chunkhash:6].js',
     },
     imageUrlLoaderOption: {
-      name: 'static/images/[path][name]-[hash:6].[ext]',
+      name: 'm/images/[path][name]-[hash:6].[ext]',
     },
     miniCssExtractPluginOption: {
-      filename: 'css/[name]-[contenthash:6].css',
-      chunkFilename: 'css/[id]-[contenthash:6].css',
+      filename: 'm/css/[name]-[contenthash:6].css',
+      chunkFilename: 'm/css/[id]-[contenthash:6].css',
     },
     /**
      * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
