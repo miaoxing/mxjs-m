5997570eb0490421b1b2f15ff5d2eea3f06f4ab6
twinh <twinhuang@qq.com>
2022-05-12 15:36:01 +0800

    refactor(m): 改为通过 `wei.setConfigs` 来设置配置

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -1,20 +1,25 @@
-import {setApiUrl, setOnBeforeHttp} from '@mxjs/taro';
+import {setOnBeforeHttp} from '@mxjs/taro';
 import '@/components/Page';
 // bootstrap: 引入自定义的样式主题
 import 'fower-preset-miaoxing';
+import {wei} from '@mxjs/app';
 import login from './login';
 import config from '../../../config';
 
-// 允许修改构建包根目录下 config.js 来配置接口地址
-let userConfig = {};
-if (process.env.TARO_ENV === 'weapp') {
-  // eslint-disable-next-line no-undef
-  userConfig = __non_webpack_require__('./config.js');
-}
-
 const bootstrap = async () => {
-  // 设置请求接口的地址
-  setApiUrl(userConfig.baseApiUrl || config.url.baseApiUrl);
+  // 允许修改构建包根目录下 config.js 来配置接口地址
+  let userConfig = {};
+  if (process.env.TARO_ENV === 'weapp') {
+    // eslint-disable-next-line no-undef
+    userConfig = __non_webpack_require__('./config.js');
+    if (userConfig.baseApiUrl) {
+      config.url.baseApiUrl = userConfig.baseApiUrl;
+    }
+  }
+
+  config.url.apiPath = 'm-api';
+
+  wei.setConfigs(config);
 
   // 请求登录
   const loginResult = login();
